2c271b9d206639c58ff8d97dddaf283536449d5d
TheEpicAndale <andale2005lolwut@gmail.com>
2022-05-15 17:06:25 -0700

    Version 0.0
    05/15/2022
    Details:
    -Basic Title Screen
    -Basic ChangeScene Screen
    -Movement Between Scenes

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchScene : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+    public string periodNameOfSwitchScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        button.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        gameManager.nameOfSwitchScene = periodNameOfSwitchScene;
+        gameManager.StartGame();
+    }
+}
